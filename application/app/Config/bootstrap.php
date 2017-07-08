dc285f35a57f9f79aa3e875274117142d3dbfdd0
Joji Koike <joji.koike@gmail.com>
2017-07-08 14:33:15 +0900

    マイグレーションプラグイン追加

--- application/app/Config/bootstrap.php
+++ application/app/Config/bootstrap.php
@@ -62,6 +62,7 @@ include dirname(__FILE__) . DS . 'bootstrap' . DS . 'environments.php';
  *
  */
 CakePlugin::load('DebugKit');
+CakePlugin::load('Migrations');
 
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter . By default CakePHP bundles two filters:
@@ -91,12 +92,12 @@ Configure::write('Dispatcher.filters', array(
  */
 App::uses('CakeLog', 'Log');
 CakeLog::config('debug', array(
-	'engine' => 'File',
-	'types' => array('notice', 'info', 'debug'),
-	'file' => 'debug',
+    'engine' => 'File',
+    'types' => array('notice', 'info', 'debug'),
+    'file' => 'debug',
 ));
 CakeLog::config('error', array(
-	'engine' => 'File',
-	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
-	'file' => 'error',
+    'engine' => 'File',
+    'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
+    'file' => 'error',
 ));
