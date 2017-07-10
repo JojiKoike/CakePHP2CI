63626c02935ccdd4e2bb194f73790fdeca6cfb33
Joji Koike <joji.koike@gmail.com>
2017-07-11 06:02:38 +0900

    新規投稿と一覧表示アクションの追加

---
+++ application/app/Controller/PostsController.php
@@ -0,0 +1,28 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Posts Controller
+ */
+class PostsController extends AppController
+{
+    public $components = ['Paginator', 'RequestHandler', 'Session'];
+
+    public function index()
+    {
+        $this->set('posts', $this->Paginator->paginate());
+    }
+
+    public function add()
+    {
+        if ($this->request->is('post')) {
+            $this->Post->create($this->request->data);
+            if ($this->Post->save()) {
+                $this->Session->setFlash(__('新しい記事を受け付けました。'));
+                return $this->redirect(['action'=>'index']);
+            } elseif (!$this->Post->save()) {
+                $this->Session->
+                setFlash(__('記事の投稿に失敗しました。入力内容を確認して再度投稿してください。'));
+            }
+        }
+    }
+}
